8ddaea6d20d7ecf64f70b7a3497bbbaffc5881a7
panos5 <Panos@ioannas-mbp.home>
2015-11-06 01:43:36 +0000

    LongestLine sorting HashMap

--- LongestLinesM/src/Main.java
+++ LongestLinesM/src/Main.java
@@ -2,8 +2,11 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.security.KeyStore.Entry;
+import java.util.Comparator;
 import java.util.Map;
+import java.util.SortedSet;
 import java.util.TreeMap;
+import java.util.TreeSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -14,7 +17,6 @@ public class Main {
 	String[] textData;
 	static String path;
 	TreeMap<String ,Integer> wordsAndSize = new TreeMap<String , Integer>();
-	
 	
 	public int readNumberOfLines() throws IOException{
 		
@@ -55,24 +57,44 @@ public class Main {
 		
 		
 		for (int line = 0 ; line < textData.length ; line++){
-
-				wordsAndSize.put(textData[line], textData.length);
+		
+			
+			//System.out.println(textData[line].length());
+			wordsAndSize.put(textData[line], textData[line].length());
 		}
 		
-		for (Map.Entry<String,Integer> entry : wordsAndSize.entrySet()){
+		for (Map.Entry<String,Integer> entry : entriesSortedByValues(wordsAndSize)){
 			
-			System.out.println(entry.getKey());
+			int k = entry.getValue();
+			String v = entry.getKey();
+			System.out.println(v + "  " + k);
 			
 		}
 				
 	}
+	
+	
+	static <K,V extends Comparable<? super V>> SortedSet<Map.Entry<K,V>> entriesSortedByValues(Map<K,V> map) {
+        SortedSet<Map.Entry<K,V>> sortedEntries = new TreeSet<Map.Entry<K,V>>(
+            new Comparator<Map.Entry<K,V>>() {
+                
+            	@Override
+            	public int compare(Map.Entry<K,V> e1, Map.Entry<K,V> e2) {
+                    int res = e1.getValue().compareTo(e2.getValue());
+                    return res != 0 ? res : 1; // Special fix to preserve items with equal values
+            	}
+            }
+        );
+        sortedEntries.addAll(map.entrySet());
+        return sortedEntries;
+    }
 
 	
 	
 	public static void main(String args[]){
 		
 		//path = args[0];
-		path = "/Users/Panos/Documents/GitHub Projects/CodeEv_Challenges/NumberOfOnes/Sample_Data";
+		path = "/Users/Panos/Documents/GitHub Projects/CodeEv_Challenges/LongestLinesM/Sample_Data";
 		Main test1 = new Main();
 		
 		try {
